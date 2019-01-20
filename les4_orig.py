e602bda338a8f8dc6c67e876911cb975e3de0a43
Gitforfun <ultradey@mail.ru>
2019-01-20 21:37:23 +0300

    Отшлифована функция Удаления дубликатов. Заменил код программы под использование функции удаления. И показа счетчика удаленных файлов.

--- les4_orig.py
+++ les4_orig.py
@@ -6,10 +6,10 @@ import psutil
 import shutil
 
 
-def duplicate_file(filename):
-	if os.path.isfile(filename):
-		newfile = filename + '.dupl'
-		shutil.copy(filename, newfile)
+def duplicate_file(file):
+	if os.path.isfile(file):
+		newfile = file + '.dupl'
+		shutil.copy(file, newfile)
 		if os.path.exists(newfile):
 			print("Создан файл: {}. Перезагрузка.".format(newfile))
 			return True
@@ -29,18 +29,19 @@ def sys_info():
 	print("Текущая директория: ", os.getcwd())
 
 
-def delete_duplicates(dir):
+def delete_duplicates(select_dir):
 	count = 0
-	file_list = os.listdir(dir)
+	file_list = os.listdir(select_dir)
 	for f in file_list:
-		fullname = os.path.join(dir, f)  # \ - разделитель в win, / - разд.в unix
+		fullname = os.path.join(select_dir, f)  # \ - разделитель в win, / - разд.в unix
 		if fullname.endswith('.dupl'):
 			os.remove(fullname)
 			print("Удален файл: " + fullname)
 			count += 1
 	return count
 
-answer = '' 			# Задаем пустую переменную для цикла while
+
+answer = ''  # Задаем пустую переменную для цикла while
 fragment = '.dupl'		# назначаем фрагмент имени, который будет добавляться или искаться
 
 print("Старт программы")
@@ -57,53 +58,39 @@ while answer != 'q':
 		print("	[4] - Вывести Информацию о процессоре")
 		print("	[5] - Дублирование файлов в текущей папке")	
 		print("	[6] - Дубливароние указанного файла")	
-		print("	[7] - Удаление всех бэкапов")	
-			
+		print("	[7] - Удаление всех бэкапов")
 		do = input("Выберете номер строки [1-7]: ")
 		if not do.isdigit():
-			print("Вы ввели не числовое значение. Повторите.")
-			continue
+			print("Вы ввели не числовое значение. Перезагрузка.")
 		do = int(do)
 		if do == 1:
 			print('Содержимое текущей директории: ', os.listdir())
 		elif do == 2:
 			sys_info()
-
 		elif do == 3:
 			print("ID всех запущенный процессов: ", psutil.pids())
 		elif do == 4:
 			print("Кол-во ядер Процессора: ", psutil.cpu_count())
-		
 		elif do == 5:
 			print("Дублирование всех файлов в папке")
 			file_list = os.listdir()
 			for oldf in file_list:
 				duplicate_file(oldf)
-
 			print("Окончание копирования всех файлов в директории. Перезагрузка")
 		elif do == 6:
 			print("Дублирование указанного файла")
 			filename = input('Введите имя файла: ')
 			if duplicate_file(filename) == False:
 				print("Введены некорректные данные. Перезагрузка")
-
 		elif do == 7:
 			print("Режим удаления дубликатов в выбранной директории: ")
-			dirname = input('Укажите имя директории.\nДля текущей директроии укажите " . ": ')
-			file_list = os.listdir(dirname)
-			for f in file_list:
-				fullname = os.path.join(dirname, f)  # \ - разделитель в win, / - разд.в unix
-				if fullname.endswith('.dupl'):
-					os.remove(fullname)
-					print("Удален файл: " + fullname)
-			print("Завершение функции удаления. Перезагрузка")
+			dirname = input('Укажите имя директории:\n("точка" - текущая директория)')
+			print("Кол-во удаленных файлов: ", delete_duplicates(dirname), " Перезагрузка.")
 		else:
 			print("Ошибка ввода номера строки")
-			
 	elif answer == "n":
 		print("Перезапуск программы")
 	elif answer == "q":
 		print("Завершение работы программы")
 	else:
 		print("Некорректный ответ")
-
